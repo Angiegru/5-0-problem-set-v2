99fd346cc3c68e98d25982af64fa018b3e184174
Angiegru <angiegrullon01@gmail.com>
2023-07-31 23:31:53 -0400

    Updated modify.js

--- src/modify.js
+++ src/modify.js
@@ -4,20 +4,38 @@ const getMainHeadingText = () => {
 };
 
 const getAllMainText = () => {
-  let mainText = [...document.querySelectorAll(`.main-text`)];
-  // mainText.map(pTag => console.log(pTag.textContent)).join();
+  let str = '';
+  let mainText = document.querySelectorAll(`.main-text`);
+  const MainText = [...mainText];
+  for(let i = 0; i < MainText.length -1; i++){
+    str += `${MainText[i].textContent},`;
+}
+str += `${MainText[2].textContent}`;
+  console.log(str)
 };
 
 const setSubtitleText = () => {
+const h2 = document.getElementById('subtitle');
+h2.textContent = "This is a subtitle!";
 };
 
 const modifyDivClassList = () => {
+  const list = document.querySelector(`#modify-classes`).classList;
+  list.remove("bad-class");
+  list.add("new-class");
 };
 
 const addH2 = () => {
+  const newElement = document.createElement("h2");
+  newElement.setAttribute('id', 'h2-test')
+  newElement.textContent = 'Another one!';
+  const bodyEl = document.body;
+  bodyEl.append(newElement)
 };
 
 const removeOldInfo = () => {
+  const list = document.querySelector(`#old-info`);
+  list.remove(`#old-info`);
 };
 
 const makeAlphabet = () => {
